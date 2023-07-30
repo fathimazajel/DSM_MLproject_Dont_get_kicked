79a7d2258c7d46619fe0d2464f4670967dde20be
Fathima Zajel Liyakath <49618414+fathimazajel@users.noreply.github.com>
2023-07-30 21:08:40 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jul 30 19:40:24 2023
+
+@author: fathimazajel
+"""
+
+import input_clean_columns
+
+#Execute the function from the script 
+input_clean_columns.process_data()
